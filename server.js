9a72558d585bcd4120cddf3d6be7abe7fe95670a
jjkusowski <jjkusowski@yahoo.com>
2017-11-08 09:26:12 -0800

    minor changes to server.js following lecture.  loadDB commented out.

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 const pg = require('pg');
 const express = require('express');
 const cors = require('cors');
-const fs = require('fs');
+// const fs = require('fs');
 
 const app = express();
 const PORT = process.env.PORT;
@@ -17,40 +17,40 @@ app.get('/test', (request, response) => response.send('Hello Garrett'));
 app.get('*', (req, res) => res.redirect(CLIENT_URL));
 app.listen(PORT, () => console.log(`Listenin on PORT : ${PORT}`));
 
-loadDB();
-
-function loadBooks() {
-  // client.query('SELECT COUNT(*) FROM books')
-  //   .then(result => {
-  //     if(!parseInt(result.rows[0].count)) {
-  fs.readFile('../book-list-client/data/books.json', (err, fd) => {
-    JSON.parse(fd.toString()).forEach(ele => {
-      client.query(`
-            INSERT INTO
-            books(author, title, isbn, image_url, description)
-            VALUES ($1, $2, $3, $4, $5);
-          `,
-        [ele.author, ele.title, ele.isbn, ele.image_url, ele.description]
-      )
-        .catch(console.error);
-    })
-  })
-//   }
-// })
-}
-
-function loadDB() {
-  client.query(`
-    CREATE TABLE IF NOT EXISTS
-    books (
-      book_id SERIAL PRIMARY KEY,
-      author VARCHAR(30) NOT NULL,
-      title VARCHAR(255) NOT NULL,
-      isbn VARCHAR(30),
-      image_url VARCHAR(255),
-      description TEXT NOT NULL
-    );`
-  )
-    .then(loadBooks())
-    .catch(console.error);
-}
+// loadDB();
+//
+// function loadBooks() {
+//   // client.query('SELECT COUNT(*) FROM books')
+//   //   .then(result => {
+//   //     if(!parseInt(result.rows[0].count)) {
+//   fs.readFile('../book-list-client/data/books.json', (err, fd) => {
+//     JSON.parse(fd.toString()).forEach(ele => {
+//       client.query(`
+//             INSERT INTO
+//             books(author, title, isbn, image_url, description)
+//             VALUES ($1, $2, $3, $4, $5);
+//           `,
+//         [ele.author, ele.title, ele.isbn, ele.image_url, ele.description]
+//       )
+//         .catch(console.error);
+//     })
+//   })
+// //   }
+// // })
+// }
+//
+// function loadDB() {
+//   client.query(`
+//     CREATE TABLE IF NOT EXISTS
+//     books (
+//       book_id SERIAL PRIMARY KEY,
+//       author VARCHAR(30) NOT NULL,
+//       title VARCHAR(255) NOT NULL,
+//       isbn VARCHAR(30),
+//       image_url VARCHAR(255),
+//       description TEXT NOT NULL
+//     );`
+//   )
+//     .then(loadBooks())
+//     .catch(console.error);
+// }
